66ab98e1f9d9925bcb12661b903ce3d32a74c44c
Ronaldo <ronaldoteran2407@gmail.com>
2025-03-24 22:17:35 -0500

    Code the logic for each step

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
-const stepOne = document.getElementById("step-one");
-const stepTwo = document.getElementById("step-two");
-const stepThree = document.getElementById("step-three");
+/* STEPS */
+const stepDivs = [
+	document.getElementById("step-one"),
+	document.getElementById("step-two"),
+	document.getElementById("step-three"),
+];
+const validateSteps = [validateStepOne, validateStepTwo, validateStepThree];
+const steps = [runStepOne, runStepTwo, runStepThree];
+let step = 0; //index that will access the steps, stepDivs and validateSteps array
 
-const steps = [stepOne, stepTwo, stepThree]; 
-let step = 0; //index that will access the steps array
+/* CONTINUE BUTTON */
+const continueButton = document.createElement("button");
+const continueButtonText = document.createTextNode("Continue");
+continueButton.appendChild(continueButtonText);
+continueButton.id = "continue-button";
+continueButton.type = "button";
 
+/* INFOMRATION COLLECTED */
 const userInfo = {
 	name: null,
 	email: null,
 	topics: [],
 };
 
-/*Continue Button*/
-const continueButton = document.createElement("button");
-const continueButtonText = document.createTextNode("Continue");
-continueButton.appendChild(continueButtonText);
-continueButton.id = "continue-button";
-continueButton.type = "button";
-stepOne.appendChild(continueButton);
-
-/* EVENT LISTENERS */
-continueButton.addEventListener("click", validateStepOne);
+function runStepOne() {
+	step = 0;
+	stepDivs[step].appendChild(continueButton);
+	continueButton.addEventListener("click", validateStepOne);
+}
 
 function validateStepOne() {
 	const name = document.getElementById("name-input").value.trim();
@@ -32,13 +38,64 @@ function validateStepOne() {
 	userInfo.name = name;
 	userInfo.email = email;
 
-	nextStep();
+	nextStep(step, step + 1); //Args: (currentStepIndex, nextStepIndex)
 }
 
-function nextStep() {
-    steps[step].classList.remove('step--active');
-
-    step++;
-    steps[step].classList.add('step--active');
-    steps[step].appendChild(continueButton);
+function runStepTwo() {
+	step = 1;
+	const topicButtons = [
+		document.getElementById("software-development"),
+		document.getElementById("user-experience"),
+		document.getElementById("graphic-design"),
+	];
+	topicButtons.forEach((button) =>
+		button.addEventListener("click", toggleTopic)
+	);
 }
+
+function validateStepTwo() {
+	const topicButtons = [
+		document.getElementById("software-development"),
+		document.getElementById("user-experience"),
+		document.getElementById("graphic-design"),
+	];
+	topicButtons.forEach((topic) => {
+		if (topic.classList.contains("topic--active"))
+			userInfo.topics.push(topic.textContent);
+	});
+
+	nextStep(step, step + 1); //Args: (currentStepIndex, nextStepIndex)
+}
+
+function runStepThree() {
+	step = 2;
+	const nameSpan = document.getElementById("name-span");
+	const emailSpan = document.getElementById("email-span");
+	const topics = document.getElementById("topics");
+
+	nameSpan.textContent = userInfo.name;
+	emailSpan.textContent = userInfo.email;
+	userInfo.topics.forEach((topic) => {
+		topics.innerHTML += `<li>${topic}</li>`;
+	});
+}
+function validateStepThree() {
+	alert("✅ Success");
+}
+
+function toggleTopic(event) {
+	event.target.classList.toggle("topic--active");
+}
+
+function nextStep(currentStepIndex, nextStepIndex) {
+	stepDivs[currentStepIndex].classList.remove("step--active");
+	continueButton.removeEventListener("click", validateSteps[currentStepIndex]);
+	continueButton.remove();
+
+	stepDivs[nextStepIndex].classList.add("step--active");
+	stepDivs[nextStepIndex].appendChild(continueButton);
+	continueButton.addEventListener("click", validateSteps[nextStepIndex]);
+	steps[nextStepIndex]();
+}
+
+runStepOne();
